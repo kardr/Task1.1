3e53127415486857a6ee33a4ce44f5162eba53eb
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-07 20:58:05 +0300

    Задание 1.1

--- Task 1.1/Program.cs
+++ Task 1.1/Program.cs
@@ -10,6 +10,19 @@ namespace Task_1._1
     {
         static void Main(string[] args)
         {
+            double S;
+            Console.WriteLine("Введите первый член прогресии b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знаменатель q:");
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число членов прогресии a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            S = (b * (Math.Pow(q, a) - b)) / (q - b);
+            Console.WriteLine("Сумма членов геометрической прогресии: " + S);
+            Console.ReadKey();
         }
     }
 }
